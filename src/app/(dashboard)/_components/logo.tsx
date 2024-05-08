4b48e7b374bafbfcfe77cec7596095d989500bd0
masumrpg <mclasix@gmail.com>
2024-05-08 15:21:38 +0700

    first commit

---
+++ src/app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+const Logo = () => {
+  // return <Image height={130} width={130} alt="Logo" src="/logo.svg" />;
+  return <Image height={130} width={130} alt="Logo" src="/masumlogo.png" />;
+};
+
+export default Logo;
