a484c5cfdc53d4f3817f0402d37af64683f63214
masumrpg <mclasix@gmail.com>
2024-07-19 08:25:06 +0700

    Mencoba fix stream

--- src/app/api/stream/[videoId]/route.ts
+++ src/app/api/stream/[videoId]/route.ts
@@ -2,7 +2,6 @@ import { authorize } from "@/google/auth-google";
 import { streamFile } from "@/google/google-drive";
 import { OAuth2Client } from "google-auth-library";
 import { NextResponse } from "next/server";
-import { PassThrough } from "stream";
 
 export async function GET(req: Request, { params }: { params: { videoId: string } }) {
     const videoId = params.videoId;
@@ -11,10 +10,21 @@ export async function GET(req: Request, { params }: { params: { videoId: string 
         const authClient: OAuth2Client = await authorize();
         const fileStream = await streamFile(authClient, videoId as string);
 
-        const passThrough = new PassThrough();
-        fileStream.pipe(passThrough);
+        const readableStream = new ReadableStream({
+            start(controller) {
+                fileStream.on('data', (chunk) => {
+                    controller.enqueue(chunk);
+                });
+                fileStream.on('end', () => {
+                    controller.close();
+                });
+                fileStream.on('error', (err) => {
+                    controller.error(err);
+                });
+            }
+        });
 
-        const response = new NextResponse(passThrough as any, {
+        const response = new NextResponse(readableStream, {
             headers: {
                 "Content-Type": "application/octet-stream"
             }
